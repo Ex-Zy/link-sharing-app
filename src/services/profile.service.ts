d35c6b8ba4d2ae6fdf00b487d94e3d9215de6569
Sergey Nagornykh <serega1905@ukr.net>
2024-05-07 10:41:56 +0300

    :truck: update: rename profileDetailsService.ts => profile.service.ts

---
+++ src/services/profile.service.ts
@@ -0,0 +1,38 @@
+import { supabase } from '../adapters/supabase'
+import { PlatformLink, Profile } from '../types'
+
+export const profileService = {
+  async getProfile(id: string) {
+    try {
+      const { data, error } = await supabase.from('profiles').select('*').eq('id', id).single()
+
+      if (error) {
+        console.error('Error fetching profile:', error)
+        return
+      }
+
+      return data
+    } catch (err) {
+      console.error('Unexpected error fetching profile:', err)
+    }
+  },
+
+  async updateProfileLinks(id: string, profile: Profile, link: PlatformLink) {
+    try {
+      const { data, error } = await supabase
+        .from('profiles')
+        .update({ links: [...profile.links, link] })
+        .eq('id', id)
+        .select()
+
+      if (error) {
+        console.error('Error updating profile links:', error)
+        return
+      }
+
+      return data
+    } catch (err) {
+      console.error('Unexpected error updating profile links:', err)
+    }
+  }
+}
