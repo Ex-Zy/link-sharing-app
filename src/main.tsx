1530c2cdeb5f462b5be7a20220b6f2c62b2ba150
Sergey Nagornykh <serega1905@ukr.net>
2024-05-09 10:59:29 +0300

    :star: feat: base Login view

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import '@/index.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
 
-import App from '@/App.tsx'
+import { router } from '@/router/router'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLDivElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
